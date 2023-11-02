e4efe5043c876580016092850b6f5a1fa2e8c487
TheCrimsonTitan <antoniolansang500@gmail.com>
2023-11-02 00:31:58 -0500

    Set up requests

--- GrabBetaKey.py
+++ GrabBetaKey.py
@@ -1,10 +1,11 @@
-
+from bs4 import BeautifulSoup
+import requests
 
 def main():
 
     URL="https://forum.makemkv.com/forum/viewtopic.php?t=1053"
+    html=requests.get(URL)
 
-    print("Hello World")
-
+    print(html.text)
 if __name__ == '__main__':
     main()
